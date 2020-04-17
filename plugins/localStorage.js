a54a1a66a6fafa703d018f1e14f84fab1e94d13d
cumet04 <medalhkr@gmail.com>
2020-04-17 19:04:12 +0900

    change localstorage key

--- plugins/localStorage.js
+++ plugins/localStorage.js
@@ -3,6 +3,7 @@ import createPersistedState from "vuex-persistedstate";
 export default ({ store }) => {
   window.onNuxtReady(() => {
     createPersistedState({
+      key: "vuex-hubook",
       paths: ["settings"],
     })(store);
   });
